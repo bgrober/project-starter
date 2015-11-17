2433ce2f264b50d4198dfb69928d4d30d84d034c
Brian <bgrober@gmail.com>
2015-11-17 15:18:45 -0800

    canvas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Canvas from './canvas.js';
 
 const App = React.createClass ({
+  getInitialState() {
+    return {
+      components: []
+    };
+  },
+  beginDrawing: function() {
+    console.log('hello')
+    var coordinates = { };
+    debugger;
+    //coordinates.x = 'mouseDown x',
+    //coordinates.y = 'mouseDown y'
+
+  },
+
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <Canvas create={this.beginDrawing}/>
+      </div>
     )
   }
 });
